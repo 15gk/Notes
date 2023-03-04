ffbdb3b1134ce8c527c7aa06884f77017885ff6a
Kriti <gautamkriti89@gmail.com>
2023-03-04 23:31:02 +0545

    added addnote section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import Note from "./components/Note";
+import { useState } from "react";
+import {nanoid} from 'nanoid';
+import React from "react";
+import NotesList from "./components/NotesList";
+import AddNote from "./components/AddNote";
 
-function App() {
+const App = ()=>{
+  const [notes,setNOtes]=useState([{
+    id:nanoid(),
+    text:"This is my first note!",
+    date :"15/04/2021"
+  },
+  {
+    id:nanoid(),
+    text:"This is my first note!",
+    date :"15/04/2021"
+  },
+  {
+    id:nanoid(),
+    text:"This is my first note!",
+    date :"15/04/2021"
+  },
+  {
+    id:nanoid(),
+    text:"This is my first note!",
+    date :"15/04/2021"
+  }
+]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   <div className="container">
+    <NotesList notes={notes}/>
+  
     </div>
-  );
+    </>
+  )
 }
-
 export default App;
---
+++ src/components/NotesList.js
@@ -0,0 +1,16 @@
+import Note from "./Note";
+import AddNote from "./AddNote";
+const NotesList = ({notes}) => {
+   return(
+    <div className="notes-list">
+        {notes.map((notes)=>(
+        <Note
+         id={notes.id} 
+         text={notes.text}
+          date={notes.date}
+          />))} 
+            <AddNote/>
+    </div>
+   )
+}
+export default NotesList;
