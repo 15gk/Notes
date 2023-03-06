51cc2dbcfd41857a2bb733bcc6384660fbf73593
Kriti <gautamkriti89@gmail.com>
2023-03-06 21:45:40 +0545

    completed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
-// import Note from "./components/Note";
-import { useState } from "react";
-import {nanoid} from 'nanoid';
-import React from "react";
-import NotesList from "./components/NotesList";
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
 
-const App = ()=>{
-  const [notes,setNOtes]=useState([{
-    id:nanoid(),
-    text:"This is my first note!",
-    date :"15/04/2021"
-  },
-  {
-    id:nanoid(),
-    text:"This is my first note!",
-    date :"15/04/2021"
-  },
-  {
-    id:nanoid(),
-    text:"This is my first note!",
-    date :"15/04/2021"
-  },
-  {
-    id:nanoid(),
-    text:"This is my first note!",
-    date :"15/04/2021"
-  }
-]);
-    const addNote = (text)=>{
-      const date =new Date();
-      const newNote={
-        id:nanoid(),
-        text:text,
-        date:date.toLocaleDateString(),
-      }
-      const newNotes = [...notes,newNote];
-      setNOtes(newNotes);
-    }
+const App = () => {
+	const [notes, setNotes] = useState([
+		{
+			id: nanoid(),
+			text: 'This is my first note!',
+			date: '15/04/2021',
+		},
+		{
+			id: nanoid(),
+			text: 'This is my second note!',
+			date: '21/04/2021',
+		},
+		{
+			id: nanoid(),
+			text: 'This is my third note!',
+			date: '28/04/2021',
+		},
+		{
+			id: nanoid(),
+			text: 'This is my new note!',
+			date: '30/04/2021',
+		},
+	]);
+  const addNote = (text) => {
+		const date = new Date();
+		const newNote = {
+			id: nanoid(),
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+	};
+
+	const deleteNote = (id) => {
+		const newNotes = notes.filter((note) => note.id !== id);
+		setNotes(newNotes);
+	};
+    
   return (
     <>
    <div className="container">
-    <NotesList notes={notes} handleAddNote={addNote}/>
+    <NotesList 
+    notes={notes} 
+    handleAddNote={addNote}
+    handleDeleteNote={deleteNote}
+    />
   
     </div>
     </>
